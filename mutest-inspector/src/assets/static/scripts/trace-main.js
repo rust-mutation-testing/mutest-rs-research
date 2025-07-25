4f9efea46cd8131d9a159c7eddb7d4dd987d9f70
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2025-07-25 16:49:28 +0100

    feat: Switch file tree to traces view by default on trace page

--- mutest-inspector/src/assets/static/scripts/trace-main.js
+++ mutest-inspector/src/assets/static/scripts/trace-main.js
@@ -1,5 +1,12 @@
 import { collapse_and_show } from "./collapser.js";
+import { FileTree } from "./file-tree.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
+    let ft = new FileTree(
+        document.getElementById('file-tree-wrapper'),
+        document.getElementById('file-tree'));
+
+    ft.showTracesTab();
+
     await collapse_and_show(document.getElementById('code-table'));
 });
