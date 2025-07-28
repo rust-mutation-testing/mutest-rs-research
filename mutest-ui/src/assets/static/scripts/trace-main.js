ad32f6bfc96f91ee8d452729fa6158ba50175250
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2025-07-28 10:43:01 +0100

    feat: Add auto fetching of traces from mutation id

--- mutest-ui/src/assets/static/scripts/trace-main.js
+++ mutest-ui/src/assets/static/scripts/trace-main.js
@@ -1,5 +1,6 @@
 import { collapse_and_show } from "./collapser.js";
 import { FileTree } from "./file-tree.js";
+import { Query } from "./query.js"
 
 document.addEventListener('DOMContentLoaded', async () => {
     let ft = new FileTree(
@@ -9,4 +10,10 @@ document.addEventListener('DOMContentLoaded', async () => {
     ft.showTracesTab();
 
     await collapse_and_show(document.getElementById('code-table'));
+
+    let query = new Query(Query.queryString());
+    let response = await fetch(`/api/traces?mutation_id=${query.getAttribute('mutation_id')}`);
+    let text = await response.text();
+    ft.setLoadedTracesTab(text);
+    ft.setupTogglesForTracesTab();
 });
